bb18adf8da06b259572db139aace42d655d41970
Source-Code-007 <utsho926@gmail.com>
2023-05-07 12:19:29 +0600

    rating add in hotel

--- src/Components/Hotel/Hotel.jsx
+++ src/Components/Hotel/Hotel.jsx
@@ -15,17 +15,17 @@ const Hotel = () => {
         <div className='min-h-screen'>
             <div className='max-w-7xl mx-5 lg:mx-auto'>
                 <h2>Stay in {name}</h2>
-                <div className='grid grid-cols-2'>
+                <div className='grid grid-cols-2 gap-5'>
                     <div className='space-y-6'>
                         {hotels && hotels.map((hotel, ind) => {
-                            const { name, address, pricePerNight, photo } = hotel
-                            return <div key={ind} className='grid grid-cols-2'>
+                            const { name, address, pricePerNight, photo, rating } = hotel
+                            return <div key={ind} className='grid grid-cols-2 gap-4'>
                                 <img src={photo} alt="" />
-                                <div>
+                                <div className='flex flex-col justify-between'>
                                     <h2>{name}</h2>
                                     <p className='text-slate-500 flex items-center gap-2'> <FaLocationArrow></FaLocationArrow> {address}</p>
                                     <div className='flex justify-between'>
-                                        <p>rating</p>
+                                        <p>{rating}</p>
                                         <p className='text-red-500'>{pricePerNight}</p>
                                     </div>
                                 </div>
